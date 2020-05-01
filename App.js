57b78717de745ed8b2e519042ddd57482be537c6
Muhammad HIdayat <muhidayat03@gmail.com>
2020-05-01 13:28:11 +0700

    add custom fonts (still not working)

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 import productReducers from './store/reducers/product_reducers'
 import ShopNavigator from './navigation/shopNavigator'
+import { AppLoading } from 'expo'
+import * as Font from 'expo-font';
 
 
 const rootReducer = combineReducers({
@@ -13,7 +15,21 @@ const rootReducer = combineReducers({
 const store = createStore(rootReducer)
 
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+
+  })
+}
+
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false)
+  if (!fontLoaded) {
+    return <AppLoading
+      startAsync={fetchFonts}
+      onFinish={setFontLoaded(true)} />
+  }
   return (
     <Provider store={store}>
       <ShopNavigator />
--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -52,6 +52,7 @@ const styles = StyleSheet.create({
 
     },
     title: {
+        fontFamily: 'open-sans',
         fontSize: 18,
         marginVertical: 4
     },
